c66ba154f4c776e68f6da2fbf4fd5b297925ac71
Nikolai <nicolas.gordienko@gmail.com>
2023-05-30 22:43:51 +0200

    Remove all files

--- hexlet_django_blog/article/urls.py
+++
@@ -1,8 +0,0 @@
-# *hexlet_django_blog/article/urls.py*
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<str:tags>/<int:article_id>/', views.ArticleView.as_view(), name='article'),
-]
