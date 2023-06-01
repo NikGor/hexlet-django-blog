9f15c352b18e780c2222cfd9f2b427247010f6ba
Nikolai <nicolas.gordienko@gmail.com>
2023-06-01 22:25:14 +0200

    templates

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('article')
+    return render(request, 'articles/index.html', {'app_name': 'article'})
